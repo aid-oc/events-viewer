8cd99be88409835cdb5e538a5f9a565d31969773
Aidan O'Connell <psyaoc@gmail.com>
2020-08-26 17:52:03 +0100

    Add typeDefs, schema structure

---
+++ server/src/graph/typeDefs.js
@@ -0,0 +1,35 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+  type Poster {
+    email: String
+    image: String
+    name: String
+    phone: String
+  }
+
+  type Event {
+    description: String
+    date: String
+    guestCount: Int
+    type: String
+    postcode: String
+    address: String
+    cancelled: Boolean
+    budget: Float
+    dietary: String
+  }
+
+  type EventListing {
+    poster: Poster
+    event: Event
+  }
+
+  type Query {
+    events: [EventListing]
+  }
+
+  schema {
+    query: Query
+  }
+`;
