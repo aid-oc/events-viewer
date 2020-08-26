58897b01db6ca105ae86e451379d85fda7602b3b
Aidan O'Connell <psyaoc@gmail.com>
2020-08-26 18:11:36 +0100

    Init client using CRA, slim down generated files

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      Feast It Events Viewer
+    </div>
+  );
+}
+
+export default App;
