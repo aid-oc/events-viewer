8970f08b508b99d77192cbb6026bf15fe2b1f962
Aidan O'Connell <psyaoc@gmail.com>
2020-08-26 17:51:10 +0100

    Create node projects, configure eslint and deps for server

---
+++ server/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: ['react'],
+  rules: {},
+};
